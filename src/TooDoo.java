2bc381ef7f4932dbdc42538ca0cd9524861f1440
unknown <Noppon@TECH21.silkspan.local>
2013-11-27 11:51:23 +0700

    Firsr commit

---
+++ src/TooDoo.java
@@ -0,0 +1,11 @@
+
+public class TooDoo {
+
+	private String foo;
+	public String getFoo() {
+	  return this.foo;
+	}
+	public void setFoo(String foo) {
+	  this.foo = foo;
+	}
+}
---
+++ src/xx.java
@@ -0,0 +1,18 @@
+
+public class xx {
+
+	 public  void main(String[] args) {
+	  TooDoo x = new TooDoo();
+	  x.setFoo("x1");
+	 String test = x2(x);
+	 System.out.print(test);
+	 }
+	
+	 
+	 public String x2(TooDoo todoo){
+		 
+		 
+		return todoo.getFoo();
+	 
+	 }
+}
